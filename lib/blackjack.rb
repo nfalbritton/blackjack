0dd33bff46afdefed41d53e81c3d3e74c525442e
Nadia Fermin Albritton <ferminnadia@gmail.com>
2017-07-26 22:08:10 -0400

    Blackjack game complete

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,38 +1,46 @@
-require_relative 'card'
-require_relative 'deck'
-require_relative 'hand'
+require_relative "card"
+require_relative "deck"
+require_relative "hand"
 
-class Blackjack
-  def initialize
-    @player = Hand.new
-    @dealer = Hand.new
-    @deck = Deck.new
-    deal
-  end
+player_cards = []
+dealer_cards = []
+cards = Deck.new
 
-  def deal
-    puts "Welcome to Blackjack"
-    hand.player_hand
-    player_turn
-    dealer_turn
-  end
+player_cards = cards.deal(2, "player")
+dealer_cards = cards.deal(2, "dealer")
+player_hand = Hand.new(player_cards)
+dealer_hand = Hand.new(dealer_cards)
+playing_game = true
 
-
-  def player_turn
-    print 'Hit or Stand(H/S):'
-    player_choice = gets.chomp
-
-    if player_choice == 'h'
-      hand.hit
-    else
-      hand.player_hand
-      hand.player_score
-    end
-  end
-
-
-  def dealer_turn
-    hand.dealer_hand
-    hand.winner
-  end
-end 
+while playing_game
+  puts ("Your current score is #{player_hand.calculate_hand}")
+  puts ("Hit (H) or Stand (S)?")
+  answer = gets.chomp.upcase
+    if answer == "H"
+      player_card = cards.deal(1,"player")[0]
+      player_hand.cards.push(player_card)
+        if player_hand.calculate_hand > 21
+          puts ("Bust, sorry you lost")
+          playing_game = false
+        end
+        elsif answer == "S" && playing_game
+         while (dealer_hand.calculate_hand) < 17 do
+           dealer_card = cards.deal(1,"dealer")[0]
+           dealer_hand.cards.push(dealer_card)
+           puts ("Dealer has #{dealer_hand.calculate_hand}")
+         end
+         if dealer_hand.calculate_hand > 21
+           puts("Dealer Bust! You won!!")
+           playing_game = false
+         else
+          playing_game = false
+          if dealer_hand.calculate_hand > player_hand.calculate_hand
+            puts ("Dealer won")
+          elsif dealer_hand.calculate_hand == player_hand.calculate_hand
+            puts ("We're tied! Tie Goes to Dealer")
+          else
+            puts "You won!!"
+          end
+       end
+     end
+   end
